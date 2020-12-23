f97a24bc9d68cc894ddba56e330cceb8c5bc85ab
omarkorbi <omarkorbi@outlook.com>
2020-12-23 23:26:48 +0100

    hello CI/CD

--- src/main/java/de/tekup/discovery/DiscoveryServiceApplication.java
+++ src/main/java/de/tekup/discovery/DiscoveryServiceApplication.java
@@ -10,8 +10,7 @@ public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
-		System.out.println("omar");
-		System.out.println("omar");
+		System.out.println("Hello CI/CD");
 	}
 
 }
